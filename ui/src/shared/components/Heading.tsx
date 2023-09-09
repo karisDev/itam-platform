7c9c277b5b7fa1068ddcfaf8df73d41dceb9f90a
Aleksey Nizhgorodov <alexey@nizhgorodov.ru>
2023-09-09 15:37:27 +0300

    nav bar

--- ui/src/shared/components/Heading.tsx
+++ ui/src/shared/components/Heading.tsx
@@ -1,9 +1,35 @@
 import LogoSvg from "@/assets/logo.svg";
+import { NavLink, useLocation } from "react-router-dom";
+import SignOutSvg from "@/assets/signout.svg";
 
-const Heading = () => {
+const NavItem = ({ children, to, currentRoute }: { children: React.ReactNode; to: string; currentRoute: string }) => {
+  const isActive = currentRoute === to;
   return (
-    <header className="w-full">
-      <nav></nav>
+    <div className="flex flex-col items-center gap-2">
+    <NavLink to={to} className={`px-[12px] py-[10px] text-sm rounded-[4px] text-text-secondary hover:text-text-primary transition-colors ${isActive ? "bg-bg-tetriary" : ""}`}>{children}</NavLink>
+    </div>
+  );
+}
+const Heading = () => {
+  const currentRoute = useLocation().pathname;
+  return (
+    <header className="w-full h-[60px] flex items-center px-4 bg-bg-primary text-text-primary gap-[24px] absolute top-0 left-0">
+      <LogoSvg width={64} height={24} />
+      <div className="flex flex-grow justify-between items-center gap-4">
+      <div className="flex items-center gap-2">
+        <NavItem to={"/myteam"} currentRoute={currentRoute}>Моя команда</NavItem>
+        <NavItem to={"/profile"} currentRoute={currentRoute}>Профиль</NavItem>
+        <NavItem to={"/hackathons"} currentRoute={currentRoute}>Хакатоны</NavItem>
+        </div>
+        <div className="flex items-center gap-3">
+          <div id="avatar" className="w-[40px] h-[40px] rounded-full bg-bg-secondary"></div>
+          <div className="flex flex-col">
+            <div className="text-sm">Женя Пригожин</div>
+            <div className="text-xs text-text-secondary">3000 снарядов</div>
+          </div>
+          <button onClick={() => {}}><SignOutSvg width={24} height={24} /></button>
+        </div>
+      </div>
     </header>
   );
 };
--- ui/src/shared/hoc/PrivateRoute.tsx
+++ ui/src/shared/hoc/PrivateRoute.tsx
@@ -2,14 +2,22 @@ import { observer } from "mobx-react-lite";
 import { Navigate } from "react-router-dom";
 import React from "react";
 import AuthStore from "@/stores/AuthStore";
+import Heading from "@/components/Heading.tsx";
 
 const PrivateRoute: React.FC<React.PropsWithChildren<unknown>> = observer(({ children }) => {
   const { authState } = AuthStore;
 
   if (authState === "loading") return <></>;
   if (authState === "anonymous") return <Navigate to="/login" />;
-
-  return <>{children}</>;
+  document.body.className = "bg-bg-secondary";
+  return(
+  <div className="w-full h-full flex bg-bg-secondary">
+    <Heading />
+    <main className="w-full h-full max-w-[940px] mx-auto flex flex-col gap-4 px-4 pt-[40px]" data-testid="main-content" data-cy="main-content">
+      {children}
+    </main>
+  </div>
+  );
 });
 
 export default PrivateRoute;
