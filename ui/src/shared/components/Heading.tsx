e725e2995e45778cacd361db0cbcb95562fd364e
karisDev <karjaakirill@gmail.com>
2023-09-09 14:38:36 +0300

    push heading base

---
+++ ui/src/shared/components/Heading.tsx
@@ -0,0 +1,11 @@
+import LogoSvg from "@/assets/logo.svg";
+
+const Heading = () => {
+  return (
+    <header className="w-full">
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Heading;
