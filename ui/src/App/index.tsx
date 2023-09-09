2b85cf564ea7ffeedd76eb6a9e837ce272a4d6e1
karisDev <karjaakirill@gmail.com>
2023-09-09 16:30:29 +0300

    fix private routes

--- ui/src/App/index.tsx
+++ ui/src/App/index.tsx
@@ -4,11 +4,13 @@ import "./transitions.scss";
 import { CSSTransition, SwitchTransition } from "react-transition-group";
 import LoginView from "../pages/Login";
 import PrivateRoute from "@/hoc/PrivateRoute";
+import Heading from "@/components/Heading";
 
 export const App = () => {
   const location = useLocation();
   return (
     <div className="bg-bg-primary text-text-primary text-sm w-full h-full flex flex-col">
+      <Heading />
       <SwitchTransition>
         <CSSTransition key={location.key} classNames="fade" timeout={300} unmountOnExit>
           <Routes location={location}>
--- ui/src/shared/components/Heading.tsx
+++ ui/src/shared/components/Heading.tsx
@@ -1,6 +1,7 @@
 import LogoSvg from "@/assets/logo.svg";
 import { NavLink, useLocation } from "react-router-dom";
 import SignOutSvg from "@/assets/signout.svg";
+import AuthStore from "@/stores/AuthStore";
 
 const NavItem = ({
   children,
@@ -25,8 +26,9 @@ const NavItem = ({
 
 const Heading = () => {
   const currentRoute = useLocation().pathname;
+  if (currentRoute === "/login") return null;
   return (
-    <header className="w-full h-[60px] flex items-center px-4 bg-bg-primary text-text-primary gap-[24px] absolute top-0 left-0 border border-border-primary">
+    <header className="w-full h-[60px] flex items-center px-4 bg-bg-primary text-text-primary gap-[24px] border-b-[1px] border-border-primary">
       <NavLink to={"/"}>
         <LogoSvg width={64} height={24} />
       </NavLink>
@@ -48,7 +50,7 @@ const Heading = () => {
             <div className="text-sm">Женя Пригожин</div>
             <div className="text-xs text-text-secondary">3000 снарядов</div>
           </div>
-          <button onClick={() => {}}>
+          <button onClick={() => AuthStore.logout()}>
             <SignOutSvg width={24} height={24} />
           </button>
         </div>
