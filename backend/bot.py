208631e04ccc8a46b4ba96f4c0c6e351ff0db3f0
Андрей <kondrandr2004@yandex.ru>
2023-09-10 16:56:19 +0300

    Фикс парсинга

--- backend/bot.py
+++ backend/bot.py
@@ -14,13 +14,13 @@ class Bot:
 
     def get_users(self):
         print('starting parser')
-        # url = f'https://api.telegram.org/bot{self.token}/getUpdates'
-        # response = requests.get(url)
-        # messages = response.json()["result"]
-        messages = [{"update_id":730674868,
-"message":{"message_id":1,"from":{"id":705821850,"is_bot":False,"first_name":"Andrew","username":"using_namespace","language_code":"ru"},"chat":{"id":705821850,"first_name":"Andrew","username":"using_namespace","type":"private"},"date":1694348364,"text":"/start","entities":[{"offset":0,"length":6,"type":"bot_command"}]}},{"update_id":730674869,
-"message":{"message_id":3,"from":{"id":705821850,"is_bot":False,"first_name":"Andrew","username":"using_namespace","language_code":"ru"},"chat":{"id":705821850,"first_name":"Andrew","username":"using_namespace","type":"private"},"date":1694349111,"text":"ssdf"}},{"update_id":730674870,
-"message":{"message_id":5,"from":{"id":705821850,"is_bot":False,"first_name":"Andrew","username":"using_namespace","language_code":"ru"},"chat":{"id":705821850,"first_name":"Andrew","username":"using_namespace","type":"private"},"date":1694353746,"text":"\u043e\u0430\u0435\u043e"}}]
+        url = f'https://api.telegram.org/bot{self.token}/getUpdates'
+        response = requests.get(url)
+        messages = response.json()["result"]
+#         messages = [{"update_id":730674868,
+# "message":{"message_id":1,"from":{"id":705821850,"is_bot":False,"first_name":"Andrew","username":"using_namespace","language_code":"ru"},"chat":{"id":705821850,"first_name":"Andrew","username":"using_namespace","type":"private"},"date":1694348364,"text":"/start","entities":[{"offset":0,"length":6,"type":"bot_command"}]}},{"update_id":730674869,
+# "message":{"message_id":3,"from":{"id":705821850,"is_bot":False,"first_name":"Andrew","username":"using_namespace","language_code":"ru"},"chat":{"id":705821850,"first_name":"Andrew","username":"using_namespace","type":"private"},"date":1694349111,"text":"ssdf"}},{"update_id":730674870,
+# "message":{"message_id":5,"from":{"id":705821850,"is_bot":False,"first_name":"Andrew","username":"using_namespace","language_code":"ru"},"chat":{"id":705821850,"first_name":"Andrew","username":"using_namespace","type":"private"},"date":1694353746,"text":"\u043e\u0430\u0435\u043e"}}]
         user_to_id = {}
         for message in messages:
             user_to_id[message["message"]["from"]["username"]] = message["message"]["from"]["id"]
